99ec73b79100a5e493dd17575f83825f5b2e58c6
Angelica <Angelica137@users.noreply.github.com>
2020-07-03 21:22:51 +0100

    first migration

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 
-class Pet(model.Model):
-	SEX_CHOICES = [('M', Male), ('F', 'Female')]
-  name = models.CharField(max_length=100)
-  submitter = models.CharField(max_length=100)
-  species = models.CharField(max_length=100)
-  breed = models.CharField(max_length=30, blank=True)
-  description = models.TextField()
-	sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
-	submission_date = models.DateTimeField()
-	age = models.IntergerField(null=True)
-  vaccinations = models.manyToManyField('Vaccine', blank=True)
+class Pet(models.Model):
+    SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    breed = models.CharField(max_length=30, blank=True)
+    description = models.TextField()
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True)
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
 
 class Vaccine(models.Model):
-	name = models.CharField(nax_length=50)
+    name = models.CharField(max_length=50)
