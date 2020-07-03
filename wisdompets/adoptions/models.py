6cf903f53fb312bd0e4fe50243ea1d491c5b4814
Angelica <Angelica137@users.noreply.github.com>
2020-07-03 21:09:02 +0100

    add pet model

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Pet(model.Model):
+	SEX_CHOICES = [('M', Male), ('F', 'Female')]
+  name = models.CharField(max_length=100)
+  submitter = models.CharField(max_length=100)
+  species = models.CharField(max_length=100)
+  breed = models.CharField(max_length=30, blank=True)
+  description = models.TextField()
+	sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
+	submission_date = models.DateTimeField()
+	age = models.IntergerField(null=True)
+  vaccinations = models.manyToManyField('Vaccine', blank=True)
+
+
+class Vaccine(models.Model):
+	name = models.CharField(nax_length=50)
